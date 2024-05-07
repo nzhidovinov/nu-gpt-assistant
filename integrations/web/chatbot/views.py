84d74823128d3fec18a7f8f6c682f344e092492e
nzhidovinov <n.zhidovinov@yandex.ru>
2024-05-07 11:51:12 +0300

    Added web integration

---
+++ integrations/web/chatbot/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def start(request):
+    return render(request, "start.html")
+
+
+def chatbot(request):
+    return render(request, "chatbot.html", context={'bot_name': 'AirportBot'})
